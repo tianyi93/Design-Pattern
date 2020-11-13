a2566f40d5a8b4f4ef9dfcbf5a678178cd126f06
tianyi93 <harter019@gmail.com>
2020-11-13 15:34:04 -0800

    multi thread ready singleton

--- singleton_pattern.cpp
+++ singleton_pattern.cpp
@@ -3,10 +3,20 @@
 //
 
 #include "singleton_pattern.h"
-int main(){
+#include <thread>
+
+void thread1(){
     auto singleton = Singleton::getInstance("singleton instance");
     singleton->print();
+}
+void thread2(){
     auto singleton2 = Singleton::getInstance("singleton instance2");
     singleton2->print();
+}
+int main(){
+    std::thread t1(thread1);
+    std::thread t2(thread2);
+    t1.join();
+    t2.join();
     return 0;
 }
--- singleton_pattern.h
+++ singleton_pattern.h
@@ -1,6 +1,7 @@
 //
 // Created by Tianyi Zhang on 11/9/20.
 //
+//referencer : https://refactoring.guru/design-patterns/singleton/cpp/example#example-1
 
 #ifndef DESIGN_PATTERN_SINGLETON_PATTERN_H
 #define DESIGN_PATTERN_SINGLETON_PATTERN_H
@@ -11,6 +12,7 @@
 class Singleton{
 private:
     std::string value_;
+    static std::mutex mutex_;
     static std::shared_ptr<Singleton> singleton_;
     Singleton(std::string value):value_(value){}
 
@@ -26,9 +28,11 @@ public:
  */
 std::shared_ptr<Singleton> Singleton::singleton_= nullptr;
 std::shared_ptr<Singleton> Singleton::getInstance(std::string value){
+    std::lock_guard<std::mutex> lock(mutex_);
     if(singleton_ == nullptr)
         singleton_ = std::shared_ptr<Singleton>(new Singleton(value));
     return singleton_;
 }
+std::mutex Singleton::mutex_;
 
 #endif //DESIGN_PATTERN_SINGLETON_PATTERN_H
