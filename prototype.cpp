f0369a7ecf3d9b6def37aa4ac4dfd331bdff2119
tianyi93 <harter019@gmail.com>
2020-11-16 19:25:08 -0800

    add printout

--- prototype.cpp
+++ prototype.cpp
@@ -16,3 +16,35 @@ int main(){
     return 0;
 
 }
+/*
+//printout
+LargeObject default constructor called
+LargeObject copy constructor called
+ConcretePrototype1 constructor called
+        Prototype template constructor is called
+        LargeObject copy constructor called
+        ConcretePrototype1 copy constructor called
+        PrototypeModel constructor is called
+        Prototype default constructor is called
+        Prototype operator = move assignment operator is called
+        ConcretePrototype2 constructor called
+Prototype template constructor is called
+        ConcretePrototype2 copy constructor called
+        PrototypeModel constructor is called
+        Prototype default constructor is called
+        Prototype operator = move assignment operator is called
+        LargeObject copy constructor called
+        ConcretePrototype1 copy constructor called
+        Prototype template constructor is called
+        LargeObject copy constructor called
+        ConcretePrototype1 copy constructor called
+        PrototypeModel constructor is called
+        Prototype move constructor is called
+Printing ConcretePrototype1: prototypeName : ConcretePrototype1, rarelyChangedField : this field contains info rarely changes for ConcretePrototype1!
+ConcretePrototype2 copy constructor called
+Prototype template constructor is called
+        ConcretePrototype2 copy constructor called
+        PrototypeModel constructor is called
+        Prototype move constructor is called
+Printing ConcretePrototype2: prototypeName : ConcretePrototype2 , rarelyChangedField : this field contains info rarely changes for ConcretePrototype2!
+*/
