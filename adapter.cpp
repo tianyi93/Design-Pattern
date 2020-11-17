9a34b1cbab67dec5dfc858c8c2e524c473ce6736
tianyi93 <harter019@gmail.com>
2020-11-16 19:44:53 -0800

    adapter function

---
+++ adapter.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Tianyi Zhang on 11/16/20.
+//
+#include "adapter.h"
+
+int main(){
+    Adapter adapter;
+    adapter.print();
+    return 0;
+}
---
+++ adapter.h
@@ -0,0 +1,30 @@
+//
+// Created by Tianyi Zhang on 11/16/20.
+//
+
+#ifndef DESIGN_PATTERN_ADAPTER_H
+#define DESIGN_PATTERN_ADAPTER_H
+#include <iostream>
+
+struct IClient{
+    virtual void print() = 0;
+};
+
+
+struct Adaptee{
+    void specialPrint(){
+        std::cout<<"Special print function in Adaptee"<<std::endl;
+    }
+};
+
+struct Adapter : public IClient{
+    void print() override{
+        adaptee_.specialPrint();
+    }
+
+private:
+    Adaptee adaptee_;
+};
+
+
+#endif //DESIGN_PATTERN_ADAPTER_H
